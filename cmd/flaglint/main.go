91d7ff63584f7164eb99c8b8f0f865745316de7e
Slater Lloyd <slater.lloyd@vivint.com>
2025-08-16 21:56:32 -0600

    add fail for tests

--- cmd/flaglint/main.go
+++ cmd/flaglint/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
 	fmt.Println("Running Flag Lint!")
+	var fail bool
+	flag.BoolVar(&fail, "t", false, "test val for if the init should fail")
+	flag.Parse()
+	if fail {
+		os.Exit(1)
+	}
 	os.Exit(0)
 }
