b9af829c09cf690650cf0c561c728bac00b28eed
Slater Lloyd <slater.lloyd@vivint.com>
2025-08-19 13:35:30 -0600

    updates

--- cmd/flaglint/main.go
+++ cmd/flaglint/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Println("Running Flag Lint")
 	var fail bool
-	flag.BoolVar(&fail, "t", false, "test val for if the init should fail")
+	flag.BoolVar(&fail, "fix", false, "test val for if the init should fail")
 	flag.Parse()
 	if fail {
 		os.Exit(1)
