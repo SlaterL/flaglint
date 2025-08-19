38c39c7a7d6bf84f358ad13d610df65c119f02b2
Slater Lloyd <slater.lloyd@vivint.com>
2025-08-19 14:50:50 -0600

    check for scratch

--- cmd/flaglint/main.go
+++ cmd/flaglint/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
+
+// UsesScratchImage opens the given Dockerfile and returns true if it uses "FROM scratch".
+func UsesScratchImage(path string) (bool, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		// Check if line starts with FROM and includes "scratch"
+		if strings.HasPrefix(strings.ToUpper(line), "FROM") && strings.Contains(line, "scratch") {
+			return true, nil
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return false, err
+	}
+	return false, nil
+}
 
 func main() {
 	fmt.Println("Running Flag Lint")
 	var fail bool
 	flag.BoolVar(&fail, "fix", false, "test val for if the init should fail")
 	flag.Parse()
-	if fail {
+	scratch, err := UsesScratchImage("Dockerfile")
+	if err != nil {
+		fmt.Println("ERROR")
+	}
+	if scratch {
 		os.Exit(1)
 	}
 	os.Exit(0)
